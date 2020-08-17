425d8b562bf118f022c1f890924171f8698e47a3
bg2015 <XMG3-Rel.1>
2020-08-18 06:49:26 +0800

    copy all file from https://github.com/grb2015/angular8Farmhouser
    note:
    node_modules_nodejs10_adapter.7z not be uploaded
    find it at https://github.com/grb2015/angular8Farmhouser

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import {ModuleWithProviders, NgModule} from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {AboutComponent} from "./about/about.component";
+import {HeaderComponent} from "./header/header.component";
+import {FooterComponent} from "./footer/footer.component";
+import {ContactComponent} from "./contact/contact.component";
+import {ServicesComponent} from "./services/services.component";
+import {PortfolioComponent} from "./portfolio/portfolio.component";
+import {PricingComponent} from "./pricing/pricing.component";
+import {HomeComponent} from "./home/home.component";
+import {Login} from "./login/login.component";
+import {personsettingComponent} from "./personsetting/personsetting.component";
+import { authenticatedService } from './shareService/authenticatedService';
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'header', component: HeaderComponent },
+  { path: 'footer', component: FooterComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'pricing', component: PricingComponent },
+  { path: 'home', component: HomeComponent},
+  { path: 'login', component: Login},
+  { path: 'personsetting', component: personsettingComponent,canActivate: [authenticatedService] }
+  
+];
+
+
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
+
+
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,55 @@
+import { BrowserModule,Title } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { Login } from './login/login.component';
+import { personsettingComponent } from './personsetting/personsetting.component';
+import {RouterModule} from "@angular/router";
+import {routing} from "./app-routing.module";
+import { HomeComponent } from './home/home.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {NgxCarouselModule} from "ngx-carousel";
+import { LoginService } from "./login/login.services";
+import {UserAuth} from "./shareService/usreAuth.service";
+import {authenticatedService} from "./shareService/authenticatedService";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactComponent,
+    ServicesComponent,
+    PortfolioComponent,
+    PricingComponent,
+    HomeComponent,
+    Login,
+    personsettingComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing,
+    ReactiveFormsModule,
+    NgxCarouselModule
+  ],
+  providers: [
+    UserAuth,
+    LoginService,
+    authenticatedService
+
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
