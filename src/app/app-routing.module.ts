d42a43d1873151192b660c63a53a75cea48c9e03
bg2015 <XMG3-Rel.1>
2020-08-29 21:51:34 +0800

    [add] 去掉不需要的module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import {ModuleWithProviders, NgModule} from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {AboutComponent} from "./about/about.component";
-import {HeaderComponent} from "./header/header.component";
-import {FooterComponent} from "./footer/footer.component";
-import {ContactComponent} from "./contact/contact.component";
-import {ServicesComponent} from "./services/services.component";
-import {PortfolioComponent} from "./portfolio/portfolio.component";
-import {PricingComponent} from "./pricing/pricing.component";
+// import {AboutComponent} from "./about/about.component";
+// import {HeaderComponent} from "./header/header.component";
+// import {FooterComponent} from "./footer/footer.component";
+// import {ContactComponent} from "./contact/contact.component";
+// import {ServicesComponent} from "./services/services.component";
+// import {PortfolioComponent} from "./portfolio/portfolio.component";
+// import {PricingComponent} from "./pricing/pricing.component";
 import {HomeComponent} from "./home/home.component";
-import {Login} from "./login/login.component";
-import {personsettingComponent} from "./personsetting/personsetting.component";
-import { authenticatedService } from './shareService/authenticatedService';
+// import {Login} from "./login/login.component";
+// import {personsettingComponent} from "./personsetting/personsetting.component";
+// import { authenticatedService } from './shareService/authenticatedService';
 
 
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,38 +4,38 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { ContactComponent } from './contact/contact.component';
-import { ServicesComponent } from './services/services.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
-import { PricingComponent } from './pricing/pricing.component';
-import { Login } from './login/login.component';
-import { personsettingComponent } from './personsetting/personsetting.component';
+// import { AboutComponent } from './about/about.component';
+// import { HeaderComponent } from './header/header.component';
+// import { FooterComponent } from './footer/footer.component';
+// import { ContactComponent } from './contact/contact.component';
+// import { ServicesComponent } from './services/services.component';
+// import { PortfolioComponent } from './portfolio/portfolio.component';
+// import { PricingComponent } from './pricing/pricing.component';
+// import { Login } from './login/login.component';
+// import { personsettingComponent } from './personsetting/personsetting.component';
 import {RouterModule} from "@angular/router";
 import {routing} from "./app-routing.module";
 import { HomeComponent } from './home/home.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import {NgxCarouselModule} from "ngx-carousel";
-import { LoginService } from "./login/login.services";
-import {UserAuth} from "./shareService/usreAuth.service";
+// import { LoginService } from "./login/login.services";
+// import {UserAuth} from "./shareService/usreAuth.service";
 import {httpservice} from "./shareService/httpsevice";
 import {authenticatedService} from "./shareService/authenticatedService";
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent,
-    HeaderComponent,
-    FooterComponent,
-    ContactComponent,
-    ServicesComponent,
-    PortfolioComponent,
-    PricingComponent,
+    // AboutComponent,
+    // HeaderComponent,
+    // FooterComponent,
+    // ContactComponent,
+    // ServicesComponent,
+    // PortfolioComponent,
+    // PricingComponent,
     HomeComponent,
-    Login,
-    personsettingComponent,
+    // Login,
+    // personsettingComponent,
   ],
   imports: [
     BrowserModule,
@@ -46,9 +46,9 @@ import {authenticatedService} from "./shareService/authenticatedService";
     NgxCarouselModule
   ],
   providers: [
-    UserAuth,
-    LoginService,
-    authenticatedService,
+    // UserAuth,
+    // LoginService,
+    // authenticatedService,
     httpservice
 
   ],
