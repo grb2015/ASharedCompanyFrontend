fe6059dea7c4f0a196419b3a36d7f1c7d23620a0
grb2015 <949409706@qq.com>
2020-08-31 16:53:34 +0800

    [add] 更改百度统计的代码。
    note:
    应该先在百度统计里面先新建网站，然后再获取代码。参考docs/baiduTongJi.png。
    之前的代码不是先建网站后获取的，所以这里更新一下

--- src/assets/js/baiduTongJi.js
+++ src/assets/js/baiduTongJi.js
@@ -5,7 +5,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?1afc6ff20a843d8a1cc97a47d5826d54";
+  hm.src = "https://hm.baidu.com/hm.js?4eacefa399547d401d6fac3c97eeb344";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
